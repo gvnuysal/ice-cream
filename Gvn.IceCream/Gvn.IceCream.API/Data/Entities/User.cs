5f83dff4f292837d15cb62c66e103a18b6319b77
guven.uysal <gvnuysal@gmail.com>
2024-08-24 12:40:56 +0300

    Added entity

---
+++ Gvn.IceCream/Gvn.IceCream.API/Data/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gvn.IceCream.API.Data.Entities
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required,MaxLength(30)]
+        public string Name { get; set; }
+        [Required, MaxLength(100)]   
+        public string Email { get; set; }
+        [Required, MaxLength(200)]
+        public string Address { get; set; }
+        public string Salt { get; set; }
+        public string Hash { get; set; }
+    }
+}
